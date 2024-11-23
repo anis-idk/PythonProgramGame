49538de40fbdeeb80895ca3ca5e0f10efe62aed2
Anis-idk <anissahnounedz@gmail.com>
2024-11-23 19:19:08 +0000

    import the Time module to calculate the cooldown or time delay

--- PythonProgram.py
+++ PythonProgram.py
@@ -1,3 +1,4 @@
 import pygame  #library used to create games
 import math    #import math module to use mathematical operations like trigonometry , angles ...
 import random  #makes the game more unpredictable, for example, by allowing zombies to appear at random points
+import time    #offers time-related features, such as the ability to get the current cooldown or delay time.
