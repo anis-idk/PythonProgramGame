dd757cffe27c23967f9791684b48b78bc471c16b
Anis-idk <anissahnounedz@gmail.com>
2024-11-23 19:23:17 +0000

    import os module to get access other files , mostly used to import the sprits and soundtracks

--- PythonProgram.py
+++ PythonProgram.py
@@ -2,3 +2,4 @@ import pygame  #library used to create games
 import math    #import math module to use mathematical operations like trigonometry , angles ...
 import random  #makes the game more unpredictable, for example, by allowing zombies to appear at random points
 import time    #offers time-related features, such as the ability to get the current cooldown or delay time.
+import os      #manages file paths, enabling the loading of resources and images from designated directories.
