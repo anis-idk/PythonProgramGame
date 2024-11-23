7045a99b41182c12ccfa97cf8749837632debcac
Anis-idk <anissahnounedz@gmail.com>
2024-11-23 19:15:44 +0000

    import the random module to make the game more unpredictable

--- PythonProgram.py
+++ PythonProgram.py
@@ -1,2 +1,3 @@
 import pygame  #library used to create games
 import math    #import math module to use mathematical operations like trigonometry , angles ...
+import random  #makes the game more unpredictable, for example, by allowing zombies to appear at random points
