c759fbcfb8e9bac7dc259f295869a9234b495d8b
Anis-idk <anissahnounedz@gmail.com>
2024-11-23 19:26:28 +0000

    being able to store the performances of the user in JSON file , saved data ( database)

--- PythonProgram.py
+++ PythonProgram.py
@@ -3,3 +3,4 @@ import math    #import math module to use mathematical operations like trigonome
 import random  #makes the game more unpredictable, for example, by allowing zombies to appear at random points
 import time    #offers time-related features, such as the ability to get the current cooldown or delay time.
 import os      #manages file paths, enabling the loading of resources and images from designated directories.
+import json    #Used for to store data (saved data) , used especially for leaderbord
