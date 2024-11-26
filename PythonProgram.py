695565152194cccd086e891f3bd91fd26600d090
Anis-idk <anissahnounedz@gmail.com>
2024-11-26 22:52:52 +0000

    creating a rectangle for the soldier sprite, centering it on the screen and setting up its position and potential hitbox for collision.

--- PythonProgram.py
+++ PythonProgram.py
@@ -66,5 +66,5 @@ bullet_img = load_image('yellow-ball-3d.png', (15, 15))
 zombie_img = load_image('skeleton-attack.png', (50, 50))
 heart_img = load_image('heart.png', (40, 40))
 
-# Soldier and Bullet Setup
+
 soldier_rect = soldier.get_rect(center=(WIDTH // 2, HEIGHT // 2))
